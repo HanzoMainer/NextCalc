24614d5c61dc1e69a89d5638efaa486ed1f71b00
HanzoMainer <154408190+HanzoMainer@users.noreply.github.com>
2024-11-02 22:26:18 +0300

    Сверстал Header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,15 +1,27 @@
 import styles from "./style.module.css";
-export function Header() 
-{
-  return (
-    <>
-        <div className={styles.headerContainer}>
-            <div className={styles.headerName}>    
-                <div className="logo"><img src="Logo.svg" alt="Logo.svg"/></div>        
-                <div><h1>NextCalc</h1></div>
+export function Header() {
+    return (
+        <>
+            <div className={styles.headerContainer}>
+                <div className={styles.headerLogo}>
+                    <div>
+                        <img src="Logo.svg" alt="Логотип" />
+                    </div>
+                    <div className={styles.headerName}>
+                        <h1>NextCalc</h1>
+                    </div>
+                </div>
+                <div className={styles.buttons}>
+                    <div className={styles.headerAuthorizationButton}>
+                        <img src="Iconly.svg" alt="Авторизация" />
+                        Авторизация
+                    </div>
+                    <div className={styles.headerRegistrationButton}>
+                        <img src="Iconly(1).svg" alt="Регистрация" />
+                        Регистрация
+                    </div>
+                </div>
             </div>
-
-        </div>
-    </>
-  )
+        </>
+    );
 }
