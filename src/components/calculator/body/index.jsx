1b1b5abce6ec645243c763db7ed4951b4dce2cb0
HanzoMainer <tym.yar1441@gmail.com>
2024-11-30 21:09:02 +0300

    Создан ввод цифр и поддержка клавиатуры

--- src/components/calculator/body/index.jsx
+++ src/components/calculator/body/index.jsx
@@ -1,6 +1,9 @@
 import styles from "./style.module.css";
 
 export function CalculatorBody({ onButtonClick }) {
+    (value) => {
+        onButtonClick(value);
+    };
     return (
         <>
             <div className={styles.calculatorButtonsContainer}>
@@ -16,61 +19,112 @@ export function CalculatorBody({ onButtonClick }) {
                 <div className={styles.calculatorSinButtons}>
                     <div className={styles.calculatorSin}>tg</div>
                 </div>
-                <div className={styles.calculatorAcButtons}>
+                <div
+                    className={styles.calculatorAcButtons}
+                    onClick={() => onButtonClick("")}
+                >
                     <div className={styles.calculatorAc}>Ac</div>
                 </div>
-                <div className={styles.calculatorAcButtons}>
+                <div
+                    className={styles.calculatorAcButtons}
+                    onClick={() => onButtonClick("r")}
+                >
                     <div className={styles.deleteButton}>
                         <img src="DeleteButton.svg" alt="Стереть" />
                     </div>
                 </div>
-                <div className={styles.calculatorOperationsButtons}>
+                <div
+                    className={styles.calculatorOperationsButtons}
+                    onClick={() => onButtonClick("/")}
+                >
                     <div className={styles.calculatorOperations}>
                         <img src="DignityButton.svg" alt="Разделить" />
                     </div>
                 </div>
-                <div className={styles.calculatorOperationsButtons}>
+                <div
+                    className={styles.calculatorOperationsButtons}
+                    onClick={() => onButtonClick("*")}
+                >
                     <div className={styles.calculatorOperations}>*</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons} onClick={() => onButtonClick("7")}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("7")}
+                >
                     <div className={styles.calculatorNumbers}>7</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("8")}
+                >
                     <div className={styles.calculatorNumbers}>8</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("9")}
+                >
                     <div className={styles.calculatorNumbers}>9</div>
                 </div>
-                <div className={styles.calculatorOperationsButtons}>
+                <div
+                    className={styles.calculatorOperationsButtons}
+                    onClick={() => onButtonClick("-")}
+                >
                     <div className={styles.calculatorOperations}>-</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("4")}
+                >
                     <div className={styles.calculatorNumbers}>4</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("5")}
+                >
                     <div className={styles.calculatorNumbers}>5</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("6")}
+                >
                     <div className={styles.calculatorNumbers}>6</div>
                 </div>
-                <div className={styles.calculatorPlusButton}>
+                <div
+                    className={styles.calculatorPlusButton}
+                    onClick={() => onButtonClick("+")}
+                >
                     <div className={styles.calculatorOperations}>+</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("1")}
+                >
                     <div className={styles.calculatorNumbers}>1</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("2")}
+                >
                     <div className={styles.calculatorNumbers}>2</div>
                 </div>
-                <div className={styles.calculatorNumbersButtons}>
+                <div
+                    className={styles.calculatorNumbersButtons}
+                    onClick={() => onButtonClick("3")}
+                >
                     <div className={styles.calculatorNumbers}>3</div>
                 </div>
                 <div className={styles.emptyPoint}></div>
-                <div className={styles.calculatorZeroButton}>
+                <div
+                    className={styles.calculatorZeroButton}
+                    onClick={() => onButtonClick("0")}
+                >
                     <div className={styles.calculatorNumbers}>0</div>
                 </div>
                 <div className={styles.emptyPoint}></div>
-                <div className={styles.calculatorDotButton}>
+                <div
+                    className={styles.calculatorDotButton}
+                    onClick={() => onButtonClick(".")}
+                >
                     <div className={styles.calculatorNumbers}>.</div>
                 </div>
                 <div className={styles.calculatorEqButton}>
--- src/components/calculator/index.jsx
+++ src/components/calculator/index.jsx
@@ -1,13 +1,45 @@
 import styles from "./style.module.css";
 import { CalculatorHead } from "./head";
-import { CalculatorBody } from "./body"
-import React, { useState } from "react";
+import { CalculatorBody } from "./body";
+import React, { useState, useEffect } from "react";
 
 export function Calculator() {
     const [displayValue, setDisplayValue] = useState("EXAMPLE");
     const handleButtonClick = (value) => {
-      setDisplayValue(value);
+        if (value == "") setDisplayValue("");
+        else if (value == "r") setDisplayValue((prev) => prev.slice(0, -1));
+        else setDisplayValue((prev) => prev + value);
     };
+    const handleKeyPress = (event) => {
+        const key = event.key;
+        if (!isNaN(key)) {
+            handleButtonClick(key);
+        } else if (key === "+") {
+            handleButtonClick("+");
+        } else if (key === "-") {
+            handleButtonClick("-");
+        } else if (key === "*") {
+            handleButtonClick("*");
+        } else if (key === "/") {
+            handleButtonClick("/");
+        } else if (key === ".") {
+            handleButtonClick(".");
+        } else if (key === "Backspace") {
+            handleButtonClick("r");
+        } else if (key.toLowerCase() === "c") {
+            handleButtonClick("");
+        } else if (key === "Enter" || key === "=") {
+            console.log('Нажата клавиша "равно"');
+        }
+    };
+
+    useEffect(() => {
+        const handleKeyDown = (event) => handleKeyPress(event);
+        window.addEventListener("keydown", handleKeyDown);
+        return () => {
+            window.removeEventListener("keydown", handleKeyDown);
+        };
+    });
 
     return (
         <>
@@ -15,7 +47,7 @@ export function Calculator() {
                 <div className={styles.calculator}>
                     <div className={styles.calculatorMiddleLine}></div>
                     <div className={styles.calculatorComponentLeft}>
-                        <CalculatorHead  displayValue={displayValue} />
+                        <CalculatorHead displayValue={displayValue} />
                         <CalculatorBody onButtonClick={handleButtonClick} />
                     </div>
                     <div className={styles.calculatorComponentRight}></div>
